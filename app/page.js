3f597c40a9ebfd17573c523b4e68d736cc5a1ad5
aaronBIOO <aaronnabioo@gmail.com>
2025-03-15 22:58:00 +0000

    update

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className='text-5xl text-center font-bold underline p-8 bg-indigo-400'>Hello world</h1>
+    <h1 className='text-5xl text-center font-bold underline p-8 bg-indigo-400'>Hello world hello world</h1>
 
   )
 }
